22c0e5faf4d02008a5f9ce6b1b8a99bba8ca740d
Jakub Vlcek <jakubvlcek.eu@gmail.com>
2025-05-17 17:16:25 +0200

    description for dialog

--- app/src/components/Charts/Chart.tsx
+++ app/src/components/Charts/Chart.tsx
@@ -2,6 +2,7 @@ import { Dialog, DialogTitle, DialogContent } from '../@shadcn/ui/dialog';
 import { useEffect, useState } from 'react';
 import { useBoutiqueStore } from '@/Hooks/Store/UseBoutiqueStore';
 import BinBarChart from './BinBarChart';
+import { Description } from '@radix-ui/react-dialog';
 export interface IChartProps {
   open: boolean;
   onOpenChange: (open: boolean) => void;
@@ -27,6 +28,7 @@ export default function Chart({ open, onOpenChange }: IChartProps) {
   return (
     <Dialog open={open} onOpenChange={onOpenChange}>
       <DialogContent className="min-w-screen h-[95vh] bg-gray-800 text-gray-200 opacity-100 overflow-y-auto">
+        <Description className='hidden'>Data Analysis</Description>
         <DialogTitle>Data Analysis</DialogTitle>
 
         {/* Bar Chart */}
